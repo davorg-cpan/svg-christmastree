268a018b95c052d915b40474ec6150668ac7eed6
Dave Cross <dave@dave.org.uk>
2018-12-04 08:20:40 +0000

    Prepare for release

--- lib/SVG/ChristmasTree.pm
+++ lib/SVG/ChristmasTree.pm
@@ -34,7 +34,7 @@ use Math::Trig qw[deg2rad tan];
 
 with 'MooseX::Getopt';
 
-our $VERSION = '0.0.3';
+our $VERSION = '0.0.4';
 
 # Constants that we haven't made into attributes yet
 use constant {
